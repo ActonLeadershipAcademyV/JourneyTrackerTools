e0b2fce13b89798bda1f0031d7b72c6112f0b40b
ActonLeadershipAcademyV <dylan@actonwoodlands.org>
2026-02-12 09:41:32 -0600

    Update countboxes-PRIVATE.js
    Finally uploading the "count everyone's boxes at once" approach.

--- countboxes-PRIVATE.js
+++ countboxes-PRIVATE.js
@@ -1,24 +1,15 @@
-{document.currentScript.remove();
- let redCounter = 0;let yellowCounter = 0;let tealCounter = 0;let deniedCounter = 0;let greenCounter = 0;
- for (let el of document.querySelectorAll('article.challenge-card:not(.next-up-self-scheduled):not(.not-on-badge-plan):not(.optional)')) {
-  if (el.classList.contains('overdue')) redCounter++;
-  if (el.classList.contains('completed')) greenCounter++;
-  if (el.classList.contains('late')) yellowCounter++;
-  if (el.classList.contains('in_review')) {
-   let match = el.parentElement.querySelector('div.challenge-card-body div.card-pills span.pill.peer-approvals.pending').textContent.match(/^\d+\/\d+\speer approvals?, (\d+) Denials?/);
-   if (match?.[1] > 0) {
-    deniedCounter++;
-   } else tealCounter++;
-  };
- };
-navigator.clipboard.writeText([
-    greenCounter + yellowCounter + deniedCounter
-        + " (" + greenCounter + "+" + yellowCounter + "+" + deniedCounter + ") / "
-        + (redCounter + yellowCounter + tealCounter + deniedCounter + greenCounter)
-        + " (" + redCounter + "+" + tealCounter + "+" + (yellowCounter + deniedCounter + greenCounter) + ")"
-        + " - "
-        + (greenCounter + yellowCounter + deniedCounter)
-        + "/"
-        + (redCounter + yellowCounter + tealCounter + deniedCounter + greenCounter)
-]);
-}
+(async function() {
+ document.currentScript.remove();
+	var allBoxes = [];
+	for (let user of document.querySelectorAll('div.dropdown-menu[aria-labelledby="dropdownPeopleNavigatorPill"] a')) {
+		let doc = Object.assign(document.createElement('div'), {
+			innerHTML: (await (await fetch(user.href)).text())
+		});
+		let boxes = [0, 0, 0, 0, 0];
+		for (let box of doc.querySelectorAll('article.challenge-card:not(.next-up-self-scheduled):not(.not-on-badge-plan):not(.optional)')) {
+			box.classList.contains('completed') ? boxes[0]++ : box.classList.contains('late') ? boxes[1]++ : box.classList.contains('overdue') ? boxes[3]++ : box.classList.contains('in_review') ? (box.parentElement.querySelector('div.challenge-card-body div.card-pills span.pill.peer-approvals.pending').textContent.match(/^\d+\/\d+\speer approvals?, (\d+) Denials?/)?.[1] > 0 ? boxes[2]++ : boxes[4]++) : void 0
+		}
+		allBoxes[allBoxes.length] = `${user.innerText}: ${([a,b,c]=boxes,a+b+c)} (${boxes[0]}+${boxes[1]}+${boxes[2]}) / ${eval(boxes.join`+`)} (${boxes[3]}+${boxes[4]}+${boxes[0]+boxes[1]+boxes[2]}) - ${([a,b,c]=boxes,a+b+c)}/${eval(boxes.join`+`)}`;
+	}
+	console.log(allBoxes.join('\n'));
+})();
